ca9cb4059a05cf6c12437bff7c063fe48a1a8f09
tian3010 <1727320226@qq.com>
2020-12-04 15:15:36 +0800

    验证码校验

--- shop-manager/src/main/java/com/xxxx/manager/controller/UserController.java
+++ shop-manager/src/main/java/com/xxxx/manager/controller/UserController.java
@@ -2,12 +2,14 @@ package com.xxxx.manager.controller;
 
 
 import com.github.pagehelper.util.StringUtil;
+import com.xxxx.common.enums.BaseResultEnum;
 import com.xxxx.common.pojo.Admin;
 import com.xxxx.common.result.BaseResult;
 import com.xxxx.manager.service.CookieService;
 import com.xxxx.sso.service.SSOService;
 import org.apache.dubbo.config.annotation.Reference;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -26,7 +28,16 @@ public class UserController {
     private CookieService cookieService;
     @RequestMapping("login")
     @ResponseBody
-    public BaseResult login(Admin admin, HttpServletRequest request, HttpServletResponse response) throws Exception {
+    public BaseResult login(Admin admin, HttpServletRequest request, HttpServletResponse response,String verify)  {
+        //获取验证码
+        String capText= (String) request.getSession().getAttribute("pictureVerfuKey");
+        BaseResult baseResult=new BaseResult();
+        //判断验证码是否为空或者一致
+        if (StringUtils.isEmpty(verify.trim())||!verify.trim().equals(capText)){
+            baseResult.setCode(BaseResultEnum.PASS_ERROR_03.getCode());
+            baseResult.setMessage(BaseResultEnum.PASS_ERROR_03.getMessage());
+            return baseResult;
+        }
         String ticket=ssoService.login(admin);
         if (!StringUtil.isEmpty(ticket)){
             boolean result=cookieService.setCookies(request,response,ticket);
@@ -34,7 +45,9 @@ public class UserController {
             request.getSession().setAttribute("user",admin);
             return result? BaseResult.success(): BaseResult.error();
         }
-        return BaseResult.error();
+        baseResult.setCode(BaseResultEnum.PASS_ERROR_04.getCode());
+        baseResult.setMessage(BaseResultEnum.PASS_ERROR_04.getMessage());
+        return baseResult;
     }
 
     @RequestMapping("logout")
